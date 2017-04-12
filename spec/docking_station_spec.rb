fd7fb4762b9ece1b3e9e102a4ad7d2bff9ea9886
Cristina Iannone <iannonecristina@gmail.com>
2017-04-12 22:22:48 +0100

    Add bike_spec.rb and bike.rb

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,9 +1,11 @@
 require 'docking_station'
+
 describe DockingStation do
+
   it 'expects DockingStation instances to respond to release_bike' do
     expect(DockingStation.new).to respond_to :release_bike
   end
 
-#subject = DockingStation.new
-it { is_expected.to respond_to :release_bike}
+  it { is_expected.to respond_to :release_bike}
+
 end
